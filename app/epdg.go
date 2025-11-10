64821ff4b1224bb84529b5e5af0145a1324d6834
Alopex6414 <1029535012@qq.com>
2025-11-10 22:39:28 +0800

    create ePDG application class & add .gitignore

---
+++ app/epdg.go
@@ -0,0 +1,24 @@
+package app
+
+type ePDG struct {
+}
+
+func New() *ePDG {
+	return &ePDG{}
+}
+
+func (e *ePDG) Init() {
+
+}
+
+func (e *ePDG) Start() {
+
+}
+
+func (e *ePDG) Stop() {
+
+}
+
+func (e *ePDG) Status() {
+
+}
